1ee1da59a0d1b010eb852d74f4812b96c0664168
cgandhi <chirag.gandhi@aptos.com>
2023-09-01 23:07:12 +0530

    output file

---
+++ AWSLambda-RateLimit2/scripts/deployment/7-lambda-output.tf
@@ -0,0 +1,9 @@
+output "function_name" {
+  description = "Name of the Lambda function."
+  value = aws_lambda_function.lambda_function.function_name
+}
+
+output "function_arn" {
+  description = "Lambda function ARN"
+  value = aws_lambda_function.lambda_function.arn
+}
